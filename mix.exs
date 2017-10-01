5e774d18767fef7994614dde5d5d6526d02a43e8
Tim Mecklem <timothy@mecklem.com>
2017-09-30 20:35:34 -0400

    Add CI, static code analysis, and hex package information

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule Rfm69.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "RFM69",
+      source_url: "https://github.com/infinity-aps/elixir_rfm69",
+      description: description(),
+      package: package()
     ]
   end
 
@@ -16,6 +20,23 @@ defmodule Rfm69.Mixfile do
   end
 
   defp deps do
-    []
+    [
+      {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp description do
+    """
+    RFM69 is a library to handle sub-GHz wireless packet communication via an RFM69 chip.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Timothy Mecklem"],
+      licenses: ["MIT License"],
+      links: %{"Github" => "https://github.com/infinity-aps/elixir_rfm69"}
+    ]
   end
 end
