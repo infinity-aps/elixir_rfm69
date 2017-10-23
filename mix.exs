d10e5d2e832e06deeccb49df2dd0714d66edc729
Tim Mecklem <timothy@mecklem.com>
2017-10-23 00:15:22 -0400

    "Revert `elixir_ale` dependency, disable `mix dialyzer` and version bump for hex publish

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rfm69.Mixfile do
   def project do
     [
       app: :rfm69,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps(),
@@ -24,7 +24,7 @@ defmodule Rfm69.Mixfile do
 
   defp deps do
     [
-      {:elixir_ale, github: "tmecklem/elixir_ale", branch: "master"},
+      {:elixir_ale, "~> 1.0"},
       {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 0.5.1", only: :test, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev}
